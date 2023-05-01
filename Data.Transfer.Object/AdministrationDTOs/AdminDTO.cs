c117dc6c096dc4b79dd3b14458a0f2c524c760b4
Naamane Nouhaila <Nouhanouha@Nouhaila2023@gmail.com>
2023-05-01 02:12:39 +0100

    Commit the ADMIN dto

---
+++ Data.Transfer.Object/AdministrationDTOs/AdminDTO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Transfer.Object.AdministrationDTOs
+{
+    public class AdminDTO
+    {
+        public Guid IdAdmin { get; set; }
+        public bool IsSuperAdmin { get; set; } = false;
+
+        [StringLength(30)]
+        public string Nom { get; set; } = default!;
+
+        [StringLength(30)]
+        public string Prenom { get; set; } = default!;
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public string? UserName { get; set; }
+
+        public string? Email { get; set; }
+
+        public bool EmailConfirmed { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public bool PhoneNumberConfirmed { get; set; }
+
+        public int AccessFailedCount { get; set; }
+
+    }
+}
