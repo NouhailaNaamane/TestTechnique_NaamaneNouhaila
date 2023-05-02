6ff77c49622e3f8437066cb1f486b07963e57123
Naamane Nouhaila <Nouhanouha@Nouhaila2023@gmail.com>
2023-05-03 00:25:01 +0100

    Generic repository interface and implementation

---
+++ Data.Access.Layer/Repositories/Generic/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using Data.Access.Layer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Access.Layer.Repositories.Generic
+{
+    public abstract class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        protected readonly ApplicationContext _dbContext;
+
+        protected GenericRepository(ApplicationContext context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task Add(TEntity entity)
+        {
+            await _dbContext.Set<TEntity>().AddAsync(entity);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll()
+        {
+            return await _dbContext.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity?> GetById(int id)
+        {
+            return await _dbContext.Set<TEntity>().FindAsync(id);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+        }
+    }
+}
---
+++ Data.Access.Layer/Repositories/Generic/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Access.Layer.Repositories.Generic
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T?> GetById(int id);
+        Task<IEnumerable<T>> GetAll();
+        Task Add(T entity);
+        void Update(T entity);
+    }
+}
